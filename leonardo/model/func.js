76279bbb92725f31d978b96a3d941ed8eb0c0496
Leonardo Kenji <leonardokenjidiasitako@gmail.com>
2019-10-22 19:47:24 -0300

    SPRINT1

---
+++ leonardo/model/func.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+var funcSchema = new mongoose.Schema({
+        Name: String, 
+        LastName: String, 
+        Email: String,
+        Password: String,
+        CPF: String,
+        Telefone: String
+        
+});
+    
+
+module.exports = mongoose.model("func",funcSchema);
