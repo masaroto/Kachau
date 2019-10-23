9433fafd1c1bd85c61b77c2086daa47b1a0d3aa8
Leonardo Kenji <leonardokenjidiasitako@gmail.com>
2019-10-22 23:53:32 -0300

    SPRINT1

---
+++ leonardo/model/prod.js
@@ -0,0 +1,12 @@
+  var mongoose = require("mongoose");
+
+var prodSchema = new mongoose.Schema({
+        name: String, 
+        desc: String, 
+        preco: Number,
+        img: String,
+        qtd: Number
+});
+    
+
+module.exports = mongoose.model("prod",prodSchema);
